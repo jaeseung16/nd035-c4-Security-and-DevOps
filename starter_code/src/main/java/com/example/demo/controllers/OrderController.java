e7a5112e83a059d85d598d7d471d9a4e86ac2eaa
Jae-Seung Lee <jaeseung@gmail.com>
2021-05-06 14:04:01 -0400

    feat: Add logging to OrderController.submit()

--- starter_code/src/main/java/com/example/demo/controllers/OrderController.java
+++ starter_code/src/main/java/com/example/demo/controllers/OrderController.java
@@ -2,6 +2,8 @@ package com.example.demo.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,33 +12,33 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.persistence.Cart;
 import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.UserOrder;
-import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.OrderRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
 
 @RestController
 @RequestMapping("/api/order")
 public class OrderController {
-	
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	
 	@Autowired
 	private UserRepository userRepository;
 	
 	@Autowired
 	private OrderRepository orderRepository;
-	
-	
+
 	@PostMapping("/submit/{username}")
 	public ResponseEntity<UserOrder> submit(@PathVariable String username) {
+		logger.info("Submitting an order for username={}", username);
 		User user = userRepository.findByUsername(username);
-		if(user == null) {
+		if (user == null) {
+			logger.error("Error while submitting an order. Cannot find a user: username={}", username);
 			return ResponseEntity.notFound().build();
 		}
 		UserOrder order = UserOrder.createFromCart(user.getCart());
 		orderRepository.save(order);
+		logger.info("Submitted an order for username={} with numberOfItems={} and total={}", order.getUser().getUsername(), order.getItems().size(), order.getTotal());
 		return ResponseEntity.ok(order);
 	}
 	
